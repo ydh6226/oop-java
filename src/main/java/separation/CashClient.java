b57cda3a4f8607546c3995b2bcadf70769cdb9c3
ydh6226 <ydh6226@naver.com>
2021-08-16 00:09:08 +0900

    기능과 책임 분리 연습 보안

--- src/main/java/separation/CashClient.java
+++ src/main/java/separation/CashClient.java
@@ -2,26 +2,17 @@ package separation;
 
 import lombok.RequiredArgsConstructor;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-
 @RequiredArgsConstructor
 public class CashClient {
 
-    private final SecretKeySpec keySpec;
-    private final IvParameterSpec ivParameterSpec;
+    private final Cryptor cryptor;
     private final RestTemplate restTemplate;
 
     private String post(Request request) throws Exception {
-        TransFormCipher encryptCipher = new TransFormCipher(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
-        String cipherText = encryptCipher.encode(toJson(request));
+        String cipherText = cryptor.encode(toJson(request));
         String cipherResponse = restTemplate.post(cipherText);
-
-        TransFormCipher decodeCipher = new TransFormCipher(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec);
-        return decodeCipher.decode(cipherResponse);
+        return cryptor.decode(cipherResponse);
     }
-
 
     private String toJson(Request request) {
         return request.toString();
--- src/main/java/separation/Cryptor.java
+++ src/main/java/separation/Cryptor.java
@@ -1,5 +1,7 @@
 package separation;
 
+import lombok.RequiredArgsConstructor;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -9,30 +11,32 @@ import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
 
-public class TransFormCipher {
+@RequiredArgsConstructor
+public class Cryptor {
 
-    private final Cipher cipher;
-    private int mode;
+    private SecretKeySpec keySpec;
+    private IvParameterSpec ivParameterSpec;
 
     private static final Encoder encoder = Base64.getEncoder();
     private static final Decoder decoder = Base64.getDecoder();
 
-    public TransFormCipher(int mode, SecretKeySpec keySpec, IvParameterSpec parameterSpec) throws GeneralSecurityException {
-        Cipher cipher = Cipher.getInstance("default_cipher");
-        cipher.init(mode, keySpec, parameterSpec);
-        this.cipher = cipher;
-        this.mode = mode;
-    }
-
     public String encode(String plainText) throws GeneralSecurityException {
+        Cipher cipher = createCipher(Cipher.ENCRYPT_MODE);
         byte[] plainTextBytes = plainText.getBytes(StandardCharsets.UTF_8);
         byte[] encodeTextBytes = encoder.encode(cipher.doFinal(plainTextBytes));
         return new String(encodeTextBytes);
     }
 
     public String decode(String cipherText) throws GeneralSecurityException {
+        Cipher cipher = createCipher(Cipher.DECRYPT_MODE);
         byte[] cipherTextBytes = cipherText.getBytes(StandardCharsets.UTF_8);
         byte[] plainTextBytes = decoder.decode(cipher.doFinal(cipherTextBytes));
         return new String(plainTextBytes);
+    }
+
+    private Cipher createCipher(int mode) throws GeneralSecurityException{
+        Cipher cipher = Cipher.getInstance("default_cipher");
+        cipher.init(mode, keySpec, ivParameterSpec);
+        return cipher;
     }
 }
